b44bd3680a4ce199a3de57c534f1b69006b83742
weelloo54 <weelloo54@gmail.com>
2021-04-16 02:46:31 +0200

    Added CreateList Function and modified the students name array

--- src/assignment.c
+++ src/assignment.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct date {
 	int day ;
@@ -20,19 +21,30 @@ struct date {
 
 };
 struct student {
-	char name [20] ;
+	char* name ;
     int id ;
     struct date dob ;
     int score ;
 
 };
 
+//Creating a linked list node
+   struct node {
+	   struct student* data;
+	   struct node* next;
+};
+
+void CreateList(struct student student1);
+
 int main() {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
 
 	/*student1 information */
 
 	struct student student1 ;
-	strcpy(student1.name ,"Ahmed Nour" ) ;
+	student1.name = "Ahmed Nour";
 	student1.id=20016;
 	student1.dob.day=20;
 	student1.dob.month=5;
@@ -49,5 +61,46 @@ int main() {
 
 	   printf( "student 1 score: %d\n", student1.score);
 
-	return 0;
+	   //Create a linked list
+	   CreateList(student1);
+
+return 0;
+}
+
+
+
+
+//Function to create a linked list
+void CreateList(struct student student1){
+int n = 0;
+printf("Please enter the number of students");
+scanf("%d",&n);
+if(n>0){
+	struct node* head = NULL;
+	struct node* tmp = NULL;
+	struct node* helpnode = NULL;
+	head = (struct node*)malloc(sizeof(struct node));
+	head->data = (struct student*)malloc(sizeof(struct student));
+	head->data = &student1;
+	head->next = NULL;
+	tmp = head;
+	for(int i=2;i<=n;i++){
+		helpnode = (struct node*)malloc(sizeof(struct node));
+		helpnode->data = (struct student*)malloc(sizeof(struct student));
+		printf("Input data for node %d in the order: Name,ID,Score,Date(Day,Month,Year)",i);
+		scanf("%s",(helpnode->data->name));
+		scanf("%d",&(helpnode->data->id));
+		scanf("%d",&(helpnode->data->score));
+		scanf("%d",&(helpnode->data->dob.day));
+		scanf("%d",&(helpnode->data->dob.month));
+		scanf("%d",&(helpnode->data->dob.year));
+		helpnode->next = NULL;
+		tmp->next = helpnode;
+		tmp = tmp->next;
+}
+}
+else{
+	printf("Incorrect input");
+}
+return;
 }
